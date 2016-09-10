1f442ce1b334edb2fece51d2dcaf689a95cb2fd2
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-10 15:48:37 +0200

    Update scripts

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,13 +1,14 @@
 'use strict'
 
-var sortKeys = require('sort-keys')
+var files = require('req-all')('../lib')
 var isEqual = require('json-is-equal')
+var sortKeys = require('sort-keys')
 var format = require('util').format
 var lodash = require('lodash')
-var files = require('..')
 
 lodash.forEach(files, function (file, filename) {
   var sortedFile = sortKeys(file, {deep: true})
+  console.log(sortedFile)
   sortedFile.models = lodash.sortBy(sortedFile.models, 'name')
 
   if (!isEqual(file, sortedFile)) {
--- scripts/sort.js
+++ scripts/sort.js
@@ -1,10 +1,10 @@
 'use strict'
 
-var sortKeys = require('sort-keys')
+var files = require('req-all')('../lib')
 var jsonFuture = require('json-future')
+var sortKeys = require('sort-keys')
 var lodash = require('lodash')
 var path = require('path')
-var files = require('..')
 
 lodash.forEach(files, function (file, filename) {
   var sortedFile = sortKeys(file, {deep: true})
