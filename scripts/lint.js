1a2e16f1baf17b168caffb824cae305c44298014
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-29 20:23:01 +0200

    Add attitude sails

--- scripts/lint.js
+++ scripts/lint.js
@@ -8,11 +8,11 @@ var lodash = require('lodash')
 
 lodash.forEach(files, function (file, filename) {
   var sortedFile = sortKeys(file, {deep: true})
-  console.log(sortedFile)
   sortedFile.models = lodash.sortBy(sortedFile.models, 'name')
 
   if (!isEqual(file, sortedFile)) {
     var message = format("File '%s' is not sorted", filename)
+    console.log(sortedFile)
     throw new Error(message)
   }
 })
